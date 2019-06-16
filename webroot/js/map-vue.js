1585fa63f18e239dddef9e620f206a9e4b871ba7
Dezrael <sergej31913@gmail.com>
2019-06-16 15:47:15 +0300

    Version 2.1

--- webroot/js/map-vue.js
+++ webroot/js/map-vue.js
@@ -7,6 +7,7 @@ var map_app = new Vue ({
 		area: '',
 		description: '',
 		code: '',
+		displayPhoto: true
 	},
 	template: `<div id="map-service" class="box box-default clearfix">
     	<div id="map"></div>
@@ -16,7 +17,7 @@ var map_app = new Vue ({
     		<p><b>Численность населения (чел.):</b> {{ population }} </p>
     		<p><b>Плотность населения (чел./км<sup>кв</sup>):</b> {{ density }} </p>
     		<p><b>Площадь (км<sup>кв</sup>):</b> {{ area }} </p>
-    		<img :src="'/img/maps/' + this.code + '.jpg'" width="100%" alt="Карта" style="margin: 2%">
+    		<img :src="'/img/maps/' + this.code + '.jpg'" width="100%" alt="Достопримечательности" onload = "this.style.display = 'block'" onerror = "this.style.display = 'none'" style="margin: 2%; display:block;">
     		<p><b>Описание:</b> {{ description }} </p>
     	</div>
     	<div v-else="" id="map-description"> <b> Выберите регион </b> </div>  
